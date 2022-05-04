474d9778c5103a0808245952c26edc21560419d3
Jwalit Shah <jwalitshah@catalyst-au.net>
2022-05-04 15:22:28 +1000

    New monitor setup

--- monitor.sh
+++ monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 964x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-4-4 --off --output DVI-I-3-3 --off --output DVI-I-2-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP-1 --mode 1920x1080 --pos 917x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal
