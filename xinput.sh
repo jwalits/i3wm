2eec3486dfd99ecba85084b59910fafdd3fdd465
Jwalit Shah <jwalitshah@catalyst-au.net>
2022-05-04 15:26:26 +1000

    New mouse setting for xinput

--- xinput.sh
+++ xinput.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mouseid="$(xinput | grep "USB OPTICAL" | cut -c 55-56)"
+mouseid="$(xinput | grep "HID 1bcf:08a0 Mouse" | cut -c 55-56)"
 xinput set-prop $mouseid 'libinput Accel Speed' -0.8
